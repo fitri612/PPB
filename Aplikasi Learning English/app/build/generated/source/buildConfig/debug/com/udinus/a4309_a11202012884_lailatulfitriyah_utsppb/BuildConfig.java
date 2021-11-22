47ad04707139c9a0aca86be3035080a634cf7d17
Hay I'am Inces <74086121+fitri612@users.noreply.github.com>
2021-11-22 09:56:30 +0700

    Learn english

---
+++ Aplikasi Learning English/app/build/generated/source/buildConfig/debug/com/udinus/a4309_a11202012884_lailatulfitriyah_utsppb/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.udinus.a4309_a11202012884_lailatulfitriyah_utsppb;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.udinus.a4309_a11202012884_lailatulfitriyah_utsppb";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
